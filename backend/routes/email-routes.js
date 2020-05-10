4767bb6692d1c8aaadb9ad9e233d234930d65dfb
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-05-10 15:23:15 +0200

    commit route changes

--- backend/routes/email-routes.js
+++ backend/routes/email-routes.js
@@ -3,7 +3,10 @@ const express = require('express');
 const emailController = require('../controllers/email-controllers');
 const router = express.Router();
 
+//Send email to Guest
 router.post('/send', emailController.sendEmail);
+
+//Send email to reset password
 router.post('/reset', emailController.resetPassword);
 
 module.exports = router;
