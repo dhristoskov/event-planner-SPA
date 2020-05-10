a7f7964863f1da8b6023a34d8d0bfddc84ba8f51
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-05-10 15:28:15 +0200

    commit route changes

--- backend/routes/tasks-routes.js
+++ backend/routes/tasks-routes.js
@@ -4,10 +4,19 @@ const { check } = require('express-validator');
 const router = express.Router();
 const taskController = require('../controllers/tasks-controller');
 
+//Get all Tasks
 router.get('/', taskController.getAllTasks);
+
+//Get all Tasks in Event
 router.get('/event/:id', taskController.getTasksByEventId);
+
+//Delete Task by ID
 router.delete('/:id', taskController.deleteTask);
+
+//Update Task
 router.patch('/:id', taskController.updateStatus);
+
+//Post a Task and Validate input
 router.post('/' , 
     [
         check('task', 'Please enter a valid task').not().isEmpty().isLength({min: 3}).trim(),
